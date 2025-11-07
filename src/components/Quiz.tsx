270e4c8ab5b08b79cc0805a299fc3174f575a9be
Dominic Hoar-Weiler <dhoarwei@andrew.cmu.edu>
2025-11-07 11:29:50 -0500

    Completed Task 1, 2, and 3

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './Quiz.css'
 import QuizQuestion from '../core/QuizQuestion';
+import QuizCore from '../core/QuizCore';
 // Hint: Take advantage of the QuizQuestion interface
 
 interface QuizState {
@@ -13,38 +14,34 @@ interface QuizState {
 const Quiz: React.FC = () => {
   // TODO: Task1 - Seprate the logic of quiz from the UI.
   // Hint: Take advantage of QuizCore to manage quiz state separately from the UI.
-  const initialQuestions: QuizQuestion[] = [
-    {
-      question: 'What is the capital of France?',
-      options: ['London', 'Berlin', 'Paris', 'Madrid'],
-      correctAnswer: 'Paris',
-    },
-  ];
-  const [state, setState] = useState<QuizState>({
-    questions: initialQuestions,
-    currentQuestionIndex: 0,  // Initialize the current question index.
-    selectedAnswer: null,  // Initialize the selected answer.
-    score: 0,  // Initialize the score.
-  });
+  const [quizCore] = useState(() => new QuizCore());
+
+  const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
 
   const handleOptionSelect = (option: string): void => {
-    setState((prevState) => ({ ...prevState, selectedAnswer: option }));
+    //setState((prevState) => ({ ...prevState, selectedAnswer: option }));
+    quizCore.answerQuestion(option);
+    setSelectedAnswer(option);
   }
 
 
   const handleButtonClick = (): void => {
     // TODO: Task3 - Implement the logic for button click ("Next Question" and "Submit").
     // Hint: You might want to check for a function in the core logic to help with this.
+      quizCore.nextQuestion();
+      setSelectedAnswer(null);
+      const currentQuestion = quizCore.getCurrentQuestion();
   } 
 
-  const { questions, currentQuestionIndex, selectedAnswer, score } = state;
-  const currentQuestion = questions[currentQuestionIndex];
+  const score = quizCore.getScore();
+  const currentQuestion = quizCore.getCurrentQuestion();
+  const num_questions = quizCore.getTotalQuestions();
 
   if (!currentQuestion) {
     return (
       <div>
         <h2>Quiz Completed</h2>
-        <p>Final Score: {score} out of {questions.length}</p>
+        <p>Final Score: {score} out of {num_questions}</p>
       </div>
     );
   }
